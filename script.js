c262714da61548d01d4ff6cff93e2eadc29c5c3f
Mikaeldwsn <mikaels2005@gmail.com>
2024-05-13 21:48:49 +0700

    add to card function design
    design done

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle('active');
 };
 
+//buat list cart
+let openShoppingCart = document.querySelector('#shoppingCart');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let listCard = document.querySelector('.list-card');
+let quantity = document.querySelector('.quantity');
+let closeShoppingCart = document.querySelector('.close-shopping');
+
+openShoppingCart.addEventListener('click', () => {
+  body.classList.toggle('active');
+})
+
+closeShoppingCart.addEventListener('click', () => {
+  body.classList.remove('active');
+})
