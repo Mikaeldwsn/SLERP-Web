73e78acca347fffb95dcbe5d64c8a08a35b2c47b
Kezota <keziameilany2005@gmail.com>
2024-05-16 09:24:18 +0700

    fix item price calculation

--- script.js
+++ script.js
@@ -75,6 +75,7 @@ function addToCart(id) {
     item.basePrice = product.price; // Store the base price
     listCards.push(item);
   } else {
+    item.price += item.basePrice; // Add the base price to the price
     item.quantity++;
   }
 
@@ -91,7 +92,7 @@ function reloadCart() {
   listCard.innerHTML = "";
 
   listCards.forEach((value) => {
-    totalPrice += value.price * value.quantity;
+    totalPrice += value.basePrice * value.quantity;
     count += value.quantity;
 
     let newDiv = document.createElement("li");
